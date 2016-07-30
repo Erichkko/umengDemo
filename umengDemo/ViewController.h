767608778b72ee31c19fd7f57253c0aebc87bf30
erichkko <erichkko@163.com>
2016-07-30 15:49:31 +0800

    init pro
    初始化友盟demo

---
+++ umengDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  umengDemo
+//
+//  Created by wanglong on 16/7/30.
+//  Copyright © 2016年 wanglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
